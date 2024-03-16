6f5fcc477ffe5298ae2aaa3081d216375b7fe253
sachinWebsoham <142967198+sachinWebsoham@users.noreply.github.com>
2024-03-17 01:00:33 +0530

    update env file

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 // import express from "express";
 // import convert from './src/router/routerconvert.js'
 const express = require("express");
-const convert = require('./src/router/routerconvert.js');
+require("dotenv").config();
+const convert = require("./src/router/routerconvert.js");
 
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 // app.use(express.static());
-app.use('/api/v1',convert)
+app.use("/api/v1", convert);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+app.listen(PORT, () => {
+  console.log("Server is running on port 8000");
 });
-
-
-
-
